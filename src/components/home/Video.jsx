6935768b00ae68c651c7824d8e9d78c41893f7ce
MASTER-OFCODE126 <manish456712@gmail.com>
2025-09-19 23:23:40 +0530

    Fix media paths with import.meta.env.BASE_URL

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="./V1.mp4"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src={`${import.meta.env.BASE_URL}V1.mp4`}></video>
     </div>
   )
 }
