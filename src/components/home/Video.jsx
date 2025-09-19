aef86937deec90e06b2dd750c82df78c95ad3847
MASTER-OFCODE126 <manish456712@gmail.com>
2025-09-19 23:10:33 +0530

    Fix video path for GitHub Pages

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-
 const video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="/V1.mp4"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="./V1.mp4"></video>
     </div>
   )
 }
